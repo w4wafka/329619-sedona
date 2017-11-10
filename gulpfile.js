d0c999cdf0bcc2634c7bd866754682543af381c0
w4wafka <ofunduk4@gmail.com>
2017-11-10 18:12:41 +0500

    Заканчивает с автоматизацией Сервер запускает папку build/, но онлайн релоада в этом случае не происходит, стили обновляются при перезагрузке страницы, чтобы обновить html, необходимо заново прописывать команду npm run build/npm start

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,51 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var run = require("run-sequence");
+var del = require("del");
 var server = require("browser-sync").create();
+
+
+gulp.task("build", function (done) {
+  run(
+    "clean",
+    "copy",
+    "fonts",
+    "style",
+    "optimg",
+    done
+  );
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("fonts", function() {
+  gulp.src("sass/fonts/fonts.scss")
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("fonts.min.css"))
+    .pipe(gulp.dest("build/css"));
+});
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -14,13 +58,30 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("html", function() {
+  gulp.src("*.html")
+  .pipe(gulp.dest("build/"));
+});
+
+gulp.task("optimg", function() {
+  return gulp.src("img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimisationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -28,5 +89,6 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html", ["html"]);
   gulp.watch("*.html").on("change", server.reload);
 });
---
+++ js/preventdefault.js
@@ -0,0 +1,5 @@
+var button = document.getElementById("preventDefaulsItem");
+
+button.addEventListener("click", function() {
+  button.preventDefault();
+})
